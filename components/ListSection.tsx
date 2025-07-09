4322819dc956a719dfaf534dc951a660ab5e5b5e
yumi <yumiueharac@gmail.com>
2025-07-08 21:55:28 -0300

    update data, fix see all button, fix image grid and fix ptbr lang

--- components/ListSection.tsx
+++ components/ListSection.tsx
@@ -31,7 +31,7 @@ export default function ListSection({
     <section className="flex flex-col mx-10 my-5">
       <div className="flex justify-between items-center text-sm mb-5">
         {title && <h2 className="font-bold text-2xl uppercase">{title}</h2>}
-        {list.length > 4 && seeAllUrl && (
+        {data.length > 4 && seeAllUrl && (
           <Link href={seeAllUrl}>
             <button className="flex items-center">
               <span className="lowercase">{t("seeAll")}</span>
@@ -40,12 +40,13 @@ export default function ListSection({
           </Link>
         )}
       </div>
+
       <div className="flex flex-wrap gap-y-2 justify-between">
         {list.length > 0 &&
           list.map((log) => <Card cardData={log} key={log.id} />)}
 
         {fillRows &&
-          Array(Math.abs(rowMinSize - list.length))
+          Array(Math.abs(rowMinSize - (list.length % rowMinSize)))
             .fill(0)
             .map((_, i) => (
               <div key={`empty-${i}`} aria-hidden className="card__title" />
