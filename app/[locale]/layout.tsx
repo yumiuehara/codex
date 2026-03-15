564bbff522a41a8c59a7c8f26b9e7dd387739352
Yumi Uehara <yumiueharac@gmail.com>
2026-03-14 23:16:53 -0300

    misc: lang and removed unused import

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inconsolata } from "next/font/google";
 import "../globals.css";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
